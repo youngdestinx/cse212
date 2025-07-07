99f124403dbcfd48249bd0b5ee3ab4f164d404c2
Destiny Linus <youngdestinx@gmail.com>
2025-07-07 22:30:41 +0100

    cse212 week1 assignment done

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,20 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Create a double array with a size of "length" that will be passed into the function through the
+        // parameter int length in the function constructor.
 
-        return []; // replace this return statement with your own
+        // Step 2: Create the member of the array by using a loop to multiply each number lesser than "length" and
+        // other than 0 with the "number" parameter of the function.
+        
+        // step 3: Return the created double array.
+        var numbers = new double [length];
+        for (int i = 0; i < length; i++)
+        {
+            var item = number * (i + 1);
+            numbers[i] = item;
+        }
+        return numbers;
     }
 
     /// <summary>
@@ -25,9 +33,16 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Store the numbers to be rotated from the right on the list in a temporary variable.
+        // Step 2: Remove the numbers to be rotated from the list.
+        // Step 3: Add the removed numbers to the end of the list.
+
+        for (int i = 0; i < amount; i++)
+        {
+            int figure = data[data.Count -1];
+            data.RemoveAt(data.Count -1);
+            data.Insert(0, figure);
+        }
+        
     }
 }
